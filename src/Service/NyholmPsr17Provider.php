80cb5adc912c5497a33fed447694d47fb6260bf4
Koji Ogawa <pierre@lef.to>
2020-01-10 15:40:35 +0900

    Change ServerRequestCreator constructor arguments.

--- src/Service/NyholmPsr17Provider.php
+++ src/Service/NyholmPsr17Provider.php
@@ -7,33 +7,36 @@ use Nyholm\Psr7\Factory\Psr17Factory;
 use Nyholm\Psr7Server\ServerRequestCreator;
 use Slim\Interfaces\ServerRequestCreatorInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
 
 class NyholmPsr17Provider implements ServiceProviderInterface
 {
     public function register(Container $container)
     {
-        $container[Psr17Factory::class] = function (Container $c) {
+        $container[Psr17Factory::class] = static function (Container $c) {
             return new Psr17Factory();
         };
 
-        $container[ResponseFactoryInterface::class] = function (Container $c) {
+        $container[ResponseFactoryInterface::class] = static function (Container $c) {
             return $c[Psr17Factory::class];
         };
 
-        $container[StreamFactoryInterface::class] = function (Container $c) {
+        $container[StreamFactoryInterface::class] = static function (Container $c) {
             return $c[Psr17Factory::class];
         };
 
-        $container[ServerRequestCreatorInterface::class] = function (Container $c) {
+        $container[ServerRequestCreatorInterface::class] = static function (Container $c) {
             $psr17Factory = $c[Psr17Factory::class];
 
             $creator = new ServerRequestCreator(
-                $psr17Factory,
-                $psr17Factory,
-                $psr17Factory,
-                $psr17Factory
+                $c[ServerRequestFactoryInterface::class] ?? $psr17Factory,
+                $c[UriFactoryInterface::class] ?? $psr17Factory,
+                $c[UploadedFileFactoryInterface::class] ?? $psr17Factory,
+                $c[StreamFactoryInterface::class]
             );
 
             return new class ($creator) implements ServerRequestCreatorInterface {
