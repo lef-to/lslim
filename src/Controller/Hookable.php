11fda204152586dcb5f96e5bf2d454d3d0765dc0
Koji Ogawa <pierre@lef.to>
2020-01-30 17:11:14 +0900

    Hookabe traitの仕様変更

--- src/Controller/Hookable.php
+++ src/Controller/Hookable.php
@@ -2,33 +2,31 @@
 declare(strict_types=1);
 namespace LSlim\Controller;
 
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use BadMethodCallException;
 
 trait Hookable
 {
+    abstract protected function handleAction(
+        $name,
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        array $args,
+        callable $action
+    ): ResponseInterface;
+
     public function __call($name, array $arguments)
     {
         $methodName = 'action' . ucfirst($name);
         if (method_exists($this, $methodName)) {
-            $request = $arguments[0];
-            $response = $arguments[1];
-            $args = $arguments[2] ?? [];
-
-            if (method_exists($this, 'beforeAction')) {
-                $res = $this->beforeAction($name, $request, $response, $args);
-                if ($res instanceof ResponseInterface) {
-                    return $res;
-                }
-            }
-
-            $response = $this->$methodName($request, $response, $args);
-
-            if (method_exists($this, 'afterAction')) {
-                $response = $this->afterAction($name, $request, $response, $args);
-            }
-
-            return $response;
+            return $this->handleAction(
+                $name,
+                $arguments[0],
+                $arguments[1],
+                $arguments[2],
+                [ $this, $methodName ]
+            );
         }
 
         throw new BadMethodCallException($methodName . ' method is not exists.');
