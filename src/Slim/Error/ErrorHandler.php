c66e3c6090d8732da9e58ab3c204fedbd1b35e4f
Koji Ogawa <pierre@lef.to>
2020-04-01 15:59:42 +0900

    Set log level to WARNING if exception is HttpNotFoundException.

--- src/Slim/Error/ErrorHandler.php
+++ src/Slim/Error/ErrorHandler.php
@@ -6,6 +6,8 @@ use Slim\Handlers\ErrorHandler as BaseHandler;
 use Slim\Interfaces\CallableResolverInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LogLevel;
+use Slim\Exception\HttpNotFoundException;
 
 class ErrorHandler extends BaseHandler
 {
@@ -35,6 +37,12 @@ class ErrorHandler extends BaseHandler
     {
         /** @var \Psr\Log\LoggerInterface $logger */
         $logger = $this->container->get('logger');
-        $logger->critical($error);
+
+        if ($this->exception instanceof HttpNotFoundException) {
+            $level = LogLevel::WARNING;
+        } else {
+            $level = LogLevel::CRITICAL;
+        }
+        $logger->log($level, $error);
     }
 }
