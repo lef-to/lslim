278c8e93b4e222d6b786fc30d8b32e943edd0e2d
Koji Ogawa <pierre@lef.to>
2020-07-21 15:27:15 +0900

    add SameSite support.

--- src/Middleware/Session.php
+++ src/Middleware/Session.php
@@ -8,6 +8,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use SessionHandlerInterface;
 use Dflydev\FigCookies\FigResponseCookies as Cookies;
 use Dflydev\FigCookies\SetCookie;
+use Dflydev\FigCookies\Modifier\SameSite;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class Session implements MiddlewareInterface
@@ -26,6 +27,11 @@ class Session implements MiddlewareInterface
      * @var int|string|\DateTimeInterface|null
      */
     protected $expires = null;
+
+    /**
+     * @var string|null
+     */
+    protected $sameSite = 'strict';
 
     /**
      * @param string $name
@@ -51,9 +57,15 @@ class Session implements MiddlewareInterface
         return $this;
     }
 
-    public function setExpires($expires):  self
+    public function setExpires($expires): self
     {
         $this->expires = $expires;
+        return $this;
+    }
+
+    public function setSamesite($sameSite): self
+    {
+        $this->sameSite = $sameSite;
         return $this;
     }
 
@@ -87,6 +99,12 @@ class Session implements MiddlewareInterface
                             ->withPath($this->path)
                             ->withExpires($this->expires)
                             ->withHttpOnly($this->httpOnly);
+
+                        if ($this->sameSite === null) {
+                            $cookie = $cookie->withoutSameSite();
+                        } else {
+                            $cookie = $cookie->withSameSite(SameSite::fromString($this->sameSite));
+                        }
 
                         $response = Cookies::set($response, $cookie);
                     }
