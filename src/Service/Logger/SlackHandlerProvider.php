f1fa7145894da2f3da3c5bec07b3ae9c7d8fcc0d
Koji Ogawa <pierre@lef.to>
2025-07-08 20:53:21 +0900

    replace slack handler.

--- src/Service/Logger/SlackHandlerProvider.php
+++ src/Service/Logger/SlackHandlerProvider.php
@@ -4,8 +4,7 @@ declare(strict_types=1);
 
 namespace LSlim\Service\Logger;
 
-use Lefto\Monolog\Formatter\SlackFormatter;
-use Lefto\Monolog\Handler\SlackHandler;
+use Monolog\Handler\SlackWebhookHandler;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
 use Monolog\Logger;
@@ -17,48 +16,38 @@ class SlackHandlerProvider implements ServiceProviderInterface
         private $name,
         private $url,
         private $level = Level::Error,
-        protected $retryCount = 0,
-        protected ?callable $retryDelay = null,
-        protected bool $throwException = false
+        private $attachment = true,
+        private $emoji = ':boom:',
+        private $short = false,
+        private $context = true,
+        private $bubble = true,
+        private $exclude_fields = []
     ) {
     }
 
     public function register(Container $container)
     {
-        $name           = $this->name;
-        $url            = $this->url;
-        $level          = $this->level;
-        $retryCount     = $this->retryCount;
-        $retryDelay     = $this->retryDelay;
-        $throwException = $this->throwException;
-
         $container->extend(
             'logger',
-            static function (
+            function (
                 Logger $logger,
                 Container $c
-            ) use (
-                $name,
-                $url,
-                $level,
-                $retryCount,
-                $retryDelay,
-                $throwException
             ) {
-                $handler = static::createHandler($name, $url, $level, $retryCount, $retryDelay, $throwException);
+                $handler = new SlackWebhookHandler(
+                    $this->url,
+                    null,
+                    $this->name,
+                    $this->attachment,
+                    $this->emoji,
+                    $this->short,
+                    $this->context,
+                    $this->level,
+                    $this->bubble,
+                    $this->exclude_fields
+                );
                 $logger->pushHandler($handler);
                 return $logger;
             }
         );
-    }
-
-    protected static function createHandler($name, $url, $level, $retryCount, $retryDelay, $throwException)
-    {
-        $handler    = new SlackHandler($url, $level, true, $retryCount, $retryDelay, $throwException);
-        $formatter  = new SlackFormatter($name);
-
-        $handler->setFormatter($formatter);
-
-        return $handler;
     }
 }
