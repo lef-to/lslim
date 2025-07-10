38093a1d6a599f382cf798bc314db2f5406241ea
Koji Ogawa <pierre@lef.to>
2025-07-10 13:21:09 +0900

    implement get default logger.

--- src/Slim/Error/ErrorHandler.php
+++ src/Slim/Error/ErrorHandler.php
@@ -8,6 +8,7 @@ use Slim\Handlers\ErrorHandler as BaseHandler;
 use Slim\Interfaces\CallableResolverInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 use Slim\Exception\HttpNotFoundException;
 
@@ -35,16 +36,21 @@ class ErrorHandler extends BaseHandler
     /**
      * @inheritdoc
      */
+    protected function getDefaultLogger(): LoggerInterface
+    {
+        return $this->container->get('logger');
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function logError(string $error): void
     {
-        /** @var \Psr\Log\LoggerInterface $logger */
-        $logger = $this->container->get('logger');
-
         if ($this->exception instanceof HttpNotFoundException) {
             $level = LogLevel::WARNING;
         } else {
             $level = LogLevel::CRITICAL;
         }
-        $logger->log($level, $error);
+        $this->logger->log($level, $error);
     }
 }
